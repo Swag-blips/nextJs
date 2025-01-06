9ebff384b234b74596cdfb4eb2bc72dcb42da5a4
Swag-blips <coderblip@gmail.com>
2025-01-06 21:35:56 +0100

    feat:shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-import {
-
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton />
+        <Button>Sign In</Button>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <Button variant={"secondary"}>Click me</Button>
     </div>
   );
 }
