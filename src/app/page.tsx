941cd18c1197798c59ed49067b2f0c796edaa7e2
Swag-blips <coderblip@gmail.com>
2025-01-07 07:46:36 +0100

    feat:mobile navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,5 @@ import { Button } from "@/components/ui/button";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <div>
-      <SignedOut>
-        <Button>Sign In</Button>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle />
-      <Button variant={"secondary"}>Click me</Button>
-    </div>
-  );
+  return <div></div>;
 }
