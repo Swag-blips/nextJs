0b07c5569b39688032496412a709f201082293a5
Swag-blips <coderblip@gmail.com>
2025-01-10 03:07:10 +0100

    feat:create post initialization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import prisma from "@/lib/prisma";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
+  const user = await currentUser();
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
 
-  
-  return <div></div>;
+      <div className="hidden lg:block lg:col-span-4 sticky top-20">
+        who to follow
+      </div>
+    </div>
+  );
 }
